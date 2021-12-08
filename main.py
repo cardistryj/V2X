44910240e153a9803fd25393140026b107a97b73
cardistryj <547207686@qq.com>
2021-12-08 09:53:44 +0800

    opt: cover handlers

--- main.py
+++ main.py
@@ -28,38 +28,42 @@ branch_name = Repo('.').active_branch.name
 MODEL_POLICY_BRANCH_DIR = os.path.join(MODEL_POLICY_ROOT_PATH, branch_name)
 RESULT_BRANCH_DIR = os.path.join(RESULT_ROOT_PATH, branch_name)
 
-if not os.path.isdir(MODEL_POLICY_BRANCH_DIR):
-    os.mkdir(MODEL_POLICY_BRANCH_DIR)
-
-if not os.path.isdir(RESULT_BRANCH_DIR):
-    os.mkdir(RESULT_BRANCH_DIR)
-
 MODEL_POLICY_DIR = os.path.join(MODEL_POLICY_BRANCH_DIR, hash_str)
 RESULT_DIR = os.path.join(RESULT_BRANCH_DIR, hash_str)
-
-if not os.path.isdir(MODEL_POLICY_DIR):
-    os.mkdir(MODEL_POLICY_DIR)
-
-if not os.path.isdir(RESULT_DIR):
-    os.mkdir(RESULT_DIR)
 
 ##### 写入配置文件
 MODEL_CONFIG_FILE_PATH = os.path.join(MODEL_POLICY_DIR, 'params_diff.conf')
 RESULT_CONFIG_FILE_PATH = os.path.join(RESULT_DIR, 'params_diff.conf')
-with open(MODEL_CONFIG_FILE_PATH, 'w') as f:
-    f.write(params_str)
-with open(RESULT_CONFIG_FILE_PATH, 'w') as f:
-    f.write(params_str)
 
 MODEL_POLICY_PATH = os.path.join(MODEL_POLICY_DIR, MODEL_NAME)
 RESULT_PATH_TEMPLATE = os.path.join(RESULT_DIR, RESULT_NAME_TEMPLATE)
 
+if not os.path.isdir(RESULT_BRANCH_DIR):
+    os.mkdir(RESULT_BRANCH_DIR)
+if not os.path.isdir(RESULT_DIR):
+    os.mkdir(RESULT_DIR)
+with open(RESULT_CONFIG_FILE_PATH, 'w') as f:
+    f.write(params_str)
+
 def get_argv(position):
     return sys.argv[position] if len(sys.argv) > position else ''
 
-def write_result(results, result_path):
-    result_frame = pd.DataFrame(results)
-    result_frame.to_csv(result_path, index=False)
+def result_handler_cover(result_path):
+    def write_result(results):
+        result_frame = pd.DataFrame(results)
+        result_frame.to_csv(result_path, index=False)
+    return write_result
+
+def model_saver_cover(model_path):
+    def save_model(model, saver):
+        if not os.path.isdir(MODEL_POLICY_BRANCH_DIR):
+            os.mkdir(MODEL_POLICY_BRANCH_DIR)
+        if not os.path.isdir(MODEL_POLICY_DIR):
+            os.mkdir(MODEL_POLICY_DIR)
+        with open(MODEL_CONFIG_FILE_PATH, 'w') as f:
+            f.write(params_str)
+        saver(model, model_path)
+    return save_model
 
 if __name__ == '__main__':
     if get_argv(1) == '-test':
@@ -67,16 +71,17 @@ if __name__ == '__main__':
         if not test_model_path:
             print('no model path given, exiting...')
         else:
-            test_result = model.test(test_model_path, TEST_EPISODE_NUM)
             model_name = os.path.basename(test_model_path)
             test_result_path = RESULT_NAME_TEMPLATE.format(model_name[8:-4], 'test')
-            write_result(test_result, test_result_path)
+            handle_results = result_handler_cover(test_result_path)
+            model.test(test_model_path, TEST_EPISODE_NUM, handle_results)
 
     else:
-        train_result = model.train(MODEL_POLICY_PATH)
         train_result_path = RESULT_PATH_TEMPLATE.format(time_now, 'train')
-        write_result(train_result, train_result_path)
+        handle_results = result_handler_cover(train_result_path)
+        handle_models = model_saver_cover(MODEL_POLICY_PATH)
+        model.train(handle_models, handle_results)
 
-        test_result = model.test(MODEL_POLICY_PATH, TEST_EPISODE_NUM)
         test_result_path = RESULT_PATH_TEMPLATE.format(time_now, 'test')
-        write_result(test_result, test_result_path)
+        handle_results = result_handler_cover(test_result_path)
+        model.test(MODEL_POLICY_PATH, TEST_EPISODE_NUM, handle_results)
